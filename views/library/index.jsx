8171d237870cdbe78ce11e109563cbd7e9d0d1f0
Ethan Flower <ethanjamesflower@gmail.com>
2022-06-25 14:27:31 -0500

    created library of reusable styled components

---
+++ views/library/index.jsx
@@ -0,0 +1,55 @@
+import Styled from 'styled-components';
+
+export const Row = Styled.div`
+  display: flex;
+  justify-content: ${(props) => props.justify};
+  align-items: ${(props) => props.align};
+`;
+
+export const Column = Styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: ${(props) => props.justify};
+  align-items: ${(props) => props.align}
+`;
+
+export const Scroll = Styled.Column`
+  display: flex;
+  flex-direction: column;
+  justify-content: ${(props) => props.justify};
+  align-items: ${(props) => props.align};
+
+  overflow: scroll;
+`;
+
+export const Button = Styled.button`
+border-radius: 10px;
+width: 417px;
+height: 54px;
+background-color: ${(props) => props.backgroundColor || '#3e7DA0'};
+color:  ${(props) => props.color || '#E0E0E0'};
+font-size: 24px;
+border: none;
+box-shadow: 4px 4px 4px 1px rgba(0,0,0,0.4);
+margin-top: 13px;
+&:active {
+  outline: none;
+  box-shadow: none;
+}
+`;
+
+export const TextInput = Styled.input`
+  border-radius: 10px;
+  width: 417px;
+  height: 54px;
+  background-color: ${(props) => props.backgroundColor || '#3e7DA0'};
+  color:  ${(props) => props.color || '#E0E0E0'};
+  font-size: 24px;
+  border: none;
+  box-shadow: 4px 4px 4px 1px rgba(0,0,0,0.4);
+  margin-top: 13px;
+&:active {
+  outline: none;
+  box-shadow: none;
+}
+`;
